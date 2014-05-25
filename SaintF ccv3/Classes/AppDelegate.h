3a29ae128d88dfffaa7d46701ba46c91e2b50bde
Alexey Semenov <semenov@goodwix.ru>
2014-05-25 04:18:47 +0400

    Initial Commit

---
+++ SaintF ccv3/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  SaintF ccv3
+//
+//  Created by Alexey Semenov on 25/05/14.
+//  Copyright Eliphas 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ SaintF ccv3/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SaintF ccv3
+//
+//  Created by Alexey Semenov on 25/05/14.
+//  Copyright Eliphas 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
